94e29382353686e8593c8e3da84a98f5bfa4cfc7
Ling-Yun Wu <wulingyun@gmail.com>
2011-01-08 14:50:58 +0800

    Add Potential.c Add decode.infer and decode.sample methods

--- R/decode.R
+++ R/decode.R
@@ -9,3 +9,12 @@ decode.tree <- function(crf)
 
 decode.lbp <- function(crf, max.iter = 10000, cutoff = 1e-4, verbose = 0)
 	.Call("Decode_LBP", crf, max.iter, cutoff, verbose)
+
+decode.infer <- function(crf, infer, ...)
+	apply(infer(crf, ...)$node.bel, 1, which.max)
+
+decode.sample <- function(crf, sample, ...)
+{
+	s <- sample(crf, ...)
+	s[which.max(apply(s, 1, function(x) log.potential(crf, x))),]
+}
