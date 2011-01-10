2178d6e5cdc0b5553a6c4138123091c34b587b8b
wulingyun <wulingyun@localhost>
2011-01-11 00:08:15 +0800

    Add sample.gibbs method

--- R/sample.R
+++ R/sample.R
@@ -6,3 +6,6 @@ sample.chain <- function(crf, size)
 
 sample.tree <- function(crf, size)
 	.Call("Sample_Tree", crf, size)
+
+sample.gibbs <- function(crf, size, burn.in = 1000, start = apply(crf$node.pot, 1, which.max))
+	.Call("Sample_Gibbs", crf, size, burn.in, start)
