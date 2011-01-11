a87632c7b25c3773009a7382cdf527130e9e2f37
Ling-Yun Wu <wulingyun@gmail.com>
2011-01-12 00:40:24 +0800

    Add clamp.crf and clamp.reset method

---
+++ R/clamp.R
@@ -0,0 +1,46 @@
+clamp.crf <- function(crf, clamped)
+{
+	data <- list()
+	if (!is.vector(clamped) || length(clamped) != crf$n.nodes)
+		stop("'clamped' should be a vector of length ", crf$n.nodes, "!")
+	if (any(clamped > crf$n.states | clamped < 0))
+		stop("'clamped' has invalid value(s)!")
+
+	data$original <- crf
+	data$clamped <- clamped
+
+	data$node.id <- which(clamped == 0)
+	data$n.nodes <- length(data$node.id)
+	data$node.map <- rep(0, crf$n.nodes)
+	data$node.map[data$node.id] <- 1:data$n.nodes
+
+	data$edge.id <- which(clamped[crf$edges[,1]] == 0 & clamped[crf$edges[,2]] == 0)
+	data$n.edges <- length(data$edge.id)
+	data$edges <- matrix(data$node.map[crf$edges[data$edge.id,]], ncol=2)
+	data$edge.map <- rep(0, crf$n.edges)
+	data$edge.map[data$edge.id] <- 1:data$n.edges
+
+	data <- make.adj.info(data)
+
+	data$n.states <- crf$n.states[data$node.id]
+	data$max.state <- max(data$n.states)
+
+	data$node.pot <- .Call("Clamp_NodePot", data)
+	data$edge.pot <- array(crf$edge.pot[1:data$max.state, 1:data$max.state, data$edge.id], dim=c(data$max.state, data$max.state, data$n.edges))
+
+	class(data) <- c("CRF.clamped", class(data))
+	data
+}
+
+clamp.reset <- function(crf, clamped)
+{
+	if (is.na(class(crf)[1]) || class(crf)[1] != "CRF.clamped")
+		stop("'crf' is not class CRF.clamped!")
+	if (sum(xor(crf$clamped == 0, clamped == 0)) != 0)
+		stop("'clamped' has different clamped structure!")
+	if (any(clamped > crf$original$n.states | clamped < 0))
+		stop("'clamped' has invalid clamped value(s)!")
+	crf$clamped <- clamped
+	crf$node.pot <- .Call("Clamp_NodePot", crf)
+	crf
+}
