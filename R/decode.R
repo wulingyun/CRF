0af229abf8d606c32fdf8a579b426c2ed6a782df
wulingyun <wulingyun@localhost>
2011-01-12 12:16:16 +0800

    Add conditional methods

--- R/decode.R
+++ R/decode.R
@@ -7,6 +7,14 @@ decode.chain <- function(crf)
 decode.tree <- function(crf)
 	.Call("Decode_Tree", crf)
 
+decode.conditional <- function(crf, clamped, decode.method, ...)
+{
+	crf <- clamp.crf(crf, clamped)
+	decode <- clamped
+	decode[crf$node.id] <- decode.method(crf, ...)
+	decode
+}
+
 decode.lbp <- function(crf, max.iter = 10000, cutoff = 1e-4, verbose = 0)
 	.Call("Decode_LBP", crf, max.iter, cutoff, verbose)
 
--- R/sample.R
+++ R/sample.R
@@ -7,5 +7,14 @@ sample.chain <- function(crf, size)
 sample.tree <- function(crf, size)
 	.Call("Sample_Tree", crf, size)
 
+sample.conditional <- function(crf, clamped, sample.method, ...)
+{
+	crf <- clamp.crf(crf, clamped)
+	s <- sample.method(crf, ...)
+	samples <- matrix(rep(clamped, nrow(s)), nrow=nrow(s), ncol=length(clamped), byrow=TRUE)
+	samples[,crf$node.id] <- s
+	samples
+}
+
 sample.gibbs <- function(crf, size, burn.in = 1000, start = apply(crf$node.pot, 1, which.max))
 	.Call("Sample_Gibbs", crf, size, burn.in, start)
