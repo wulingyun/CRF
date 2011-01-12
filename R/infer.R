9c46eb29d9966c5d14b3de92b6b7fecc05dd2838
Ling-Yun Wu <wulingyun@gmail.com>
2011-01-12 12:16:16 +0800

    Add conditional methods

--- R/infer.R
+++ R/infer.R
@@ -7,6 +7,27 @@ infer.chain <- function(crf)
 infer.tree <- function(crf)
 	.Call("Infer_Tree", crf)
 
+infer.conditional <- function(crf, clamped, infer.method, ...)
+{
+	belief <- list()
+	belief$node.bel <- array(0, dim=c(crf$n.nodes, crf$max.state))
+	belief$edge.bel <- array(0, dim=c(crf$max.state, crf$max.state, crf$n.edges))
+	crf <- clamp.crf(crf, clamped)
+	b <- infer.method(crf, ...)
+	belief$node.bel[crf$node.id, 1:crf$max.state] <- b$node.bel
+	belief$edge.bel[1:crf$max.state, 1:crf$max.state, crf$edge.id] <- b$edge.bel
+	belief$logZ <- b$logZ
+	belief$node.bel[cbind(which(clamped != 0), clamped[clamped != 0])] <- 1
+	e <- crf$original$edges
+	e0 <- which(clamped[e[,1]] != 0 & clamped[e[,2]] != 0)
+	e1 <- which(clamped[e[,1]] != 0 & clamped[e[,2]] == 0)
+	e2 <- which(clamped[e[,1]] == 0 & clamped[e[,2]] != 0)
+	belief$edge.bel[cbind(clamped[e[e0,1]], clamped[e[e0,2]], e0)] <- 1
+	belief$edge.bel[cbind(rep(clamped[e[e1,1]], each=crf$max.state), 1:crf$max.state, rep(e1, each=crf$max.state))] <- t(b$node.bel[crf$node.map[e[e1,2]],])
+	belief$edge.bel[cbind(1:crf$max.state, rep(clamped[e[e2,2]], each=crf$max.state), rep(e2, each=crf$max.state))] <- t(b$node.bel[crf$node.map[e[e2,1]],])
+	belief
+}
+
 infer.lbp <- function(crf, max.iter = 10000, cutoff = 1e-4, verbose = 0)
 	.Call("Infer_LBP", crf, max.iter, cutoff, verbose)
 
