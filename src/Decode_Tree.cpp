dd6ec8cfb6f65e91ab28e4c52e06ec7105a0873c
wulingyun <wulingyun@localhost>
2011-01-19 23:20:25 +0800

    Remove BPtemplate.h and combine *BP and *BP_max functions

--- src/Decode_Tree.cpp
+++ src/Decode_Tree.cpp
@@ -13,7 +13,7 @@ void CRF::Decode_Tree()
 {
 	double *messages_1 = (double *) R_alloc(maxState * nEdges, sizeof(double)); // Messages from n2 to n1 at edge (n1, n2)
 	double *messages_2 = (double *) R_alloc(maxState * nEdges, sizeof(double)); // Messages from n1 to n2 at edge (n1, n2)
-	TreeBP_max(messages_1, messages_2);
+	TreeBP(messages_1, messages_2, true);
 	Message2NodeBelief(messages_1, messages_2);
 	MaxOfMarginals();
 }
