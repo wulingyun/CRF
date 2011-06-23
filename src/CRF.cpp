047409d6d76bb1985a8af8415ca933e721482e87
wulingyun <wulingyun@localhost>
2011-06-23 15:30:26 +0800

    Add Sample_Cutset.cpp Fix bug in CRF.cpp

--- src/CRF.cpp
+++ src/CRF.cpp
@@ -133,14 +133,13 @@ void CRF::Init_Samples(SEXP _size)
 {
 	PROTECT(_size = AS_INTEGER(_size));
 	Init_Samples(INTEGER_POINTER(_size)[0]);
-	UNPROTECT(1);
+	numProtect++;
 }
 
 void CRF::Set_Samples(SEXP _otherSamples)
 {
-	_samples = _otherSamples;
-	PROTECT(_samples = AS_INTEGER(_samples));
+	PROTECT(_samples = AS_INTEGER(_otherSamples));
 	samples = INTEGER_POINTER(_samples);
 	nSamples = length(_samples) / nNodes;
-	UNPROTECT(1);
+	numProtect++;
 }
