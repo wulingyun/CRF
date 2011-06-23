7b566dd2d3000a20df6c35367ed74f1e03d9a818
wulingyun <wulingyun@localhost>
2011-06-23 23:54:05 +0800

    Reduce memory usage and improve speed for cutset methods

--- src/Infer_Tree.cpp
+++ src/Infer_Tree.cpp
@@ -10,10 +10,14 @@ SEXP Infer_Tree(SEXP _crf)
 
 void CRF::Infer_Tree()
 {
+	void *vmax = vmaxget(); 
+
 	double *messages_1 = (double *) R_alloc(maxState * nEdges, sizeof(double)); // Messages from n2 to n1 at edge (n1, n2)
 	double *messages_2 = (double *) R_alloc(maxState * nEdges, sizeof(double)); // Messages from n1 to n2 at edge (n1, n2)
 	TreeBP(messages_1, messages_2);
 	Message2NodeBelief(messages_1, messages_2);
 	Message2EdgeBelief(messages_1, messages_2);
 	BetheFreeEnergy();
+
+	vmaxset(vmax); 
 }
