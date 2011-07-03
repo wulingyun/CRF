7f718ad2d1659ff65182f8c7afc3e1366179f2e9
Ling-Yun Wu <wulingyun@gmail.com>
2011-07-04 02:23:02 +0800

    Add new argument for Decode_Cutset Fix bugs in R scripts

--- R/infer.R
+++ R/infer.R
@@ -12,19 +12,19 @@ infer.conditional <- function(crf, clamped, infer.method, ...)
 	belief <- list()
 	belief$node.bel <- array(0, dim=c(crf$n.nodes, crf$max.state))
 	belief$edge.bel <- array(0, dim=c(crf$max.state, crf$max.state, crf$n.edges))
-	crf <- clamp.crf(crf, clamped)
-	b <- infer.method(crf, ...)
-	belief$node.bel[crf$node.id, 1:crf$max.state] <- b$node.bel
-	belief$edge.bel[1:crf$max.state, 1:crf$max.state, crf$edge.id] <- b$edge.bel
+	newcrf <- clamp.crf(crf, clamped)
+	b <- infer.method(newcrf, ...)
+	belief$node.bel[newcrf$node.id, 1:newcrf$max.state] <- b$node.bel
+	belief$edge.bel[1:newcrf$max.state, 1:newcrf$max.state, newcrf$edge.id] <- b$edge.bel
 	belief$logZ <- b$logZ
 	belief$node.bel[cbind(which(clamped != 0), clamped[clamped != 0])] <- 1
-	e <- crf$original$edges
+	e <- newcrf$original$edges
 	e0 <- which(clamped[e[,1]] != 0 & clamped[e[,2]] != 0)
 	e1 <- which(clamped[e[,1]] != 0 & clamped[e[,2]] == 0)
 	e2 <- which(clamped[e[,1]] == 0 & clamped[e[,2]] != 0)
 	if (length(e0) > 0) belief$edge.bel[cbind(clamped[e[e0,1]], clamped[e[e0,2]], e0)] <- 1
-	if (length(e1) > 0) belief$edge.bel[cbind(rep(clamped[e[e1,1]], each=crf$max.state), 1:crf$max.state, rep(e1, each=crf$max.state))] <- t(b$node.bel[crf$node.map[e[e1,2]],])
-	if (length(e2) > 0) belief$edge.bel[cbind(1:crf$max.state, rep(clamped[e[e2,2]], each=crf$max.state), rep(e2, each=crf$max.state))] <- t(b$node.bel[crf$node.map[e[e2,1]],])
+	if (length(e1) > 0) belief$edge.bel[cbind(rep(clamped[e[e1,1]], each=newcrf$max.state), 1:newcrf$max.state, rep(e1, each=newcrf$max.state))] <- t(b$node.bel[newcrf$node.map[e[e1,2]],])
+	if (length(e2) > 0) belief$edge.bel[cbind(1:newcrf$max.state, rep(clamped[e[e2,2]], each=newcrf$max.state), rep(e2, each=newcrf$max.state))] <- t(b$node.bel[newcrf$node.map[e[e2,1]],])
 	belief
 }
 
@@ -32,16 +32,16 @@ infer.cutset <- function(crf, cutset, is.chain = FALSE)
 {
 	clamped <- rep(0, crf$n.nodes)
 	clamped[cutset] <- 1
-	crf <- clamp.crf(crf, clamped)
-	.Call("Infer_Cutset", crf, is.chain)
+	newcrf <- clamp.crf(crf, clamped)
+	.Call("Infer_Cutset", newcrf, is.chain)
 }
 
 infer.cutsetChain <- function(crf, cutset)
 {
 	clamped <- rep(0, crf$n.nodes)
 	clamped[cutset] <- 1
-	crf <- clamp.crf(crf, clamped)
-	.Call("Infer_CutsetChain", crf)
+	newcrf <- clamp.crf(crf, clamped)
+	.Call("Infer_CutsetChain", newcrf)
 }
 
 infer.sample <- function(crf, sample.method, ...)
--- R/sample.R
+++ R/sample.R
@@ -9,10 +9,10 @@ sample.tree <- function(crf, size)
 
 sample.conditional <- function(crf, size, clamped, sample.method, ...)
 {
-	crf <- clamp.crf(crf, clamped)
-	s <- sample.method(crf, size, ...)
+	newcrf <- clamp.crf(crf, clamped)
+	s <- sample.method(newcrf, size, ...)
 	samples <- matrix(rep(clamped, nrow(s)), nrow=nrow(s), ncol=length(clamped), byrow=TRUE)
-	samples[,crf$node.id] <- s
+	samples[,newcrf$node.id] <- s
 	samples
 }
 
@@ -20,16 +20,16 @@ sample.cutset <- function(crf, size, cutset, is.chain = FALSE)
 {
 	clamped <- rep(0, crf$n.nodes)
 	clamped[cutset] <- 1
-	crf <- clamp.crf(crf, clamped)
-	.Call("Sample_Cutset", crf, size, is.chain)
+	newcrf <- clamp.crf(crf, clamped)
+	.Call("Sample_Cutset", newcrf, size, is.chain)
 }
 
 sample.cutsetChain <- function(crf, size, cutset)
 {
 	clamped <- rep(0, crf$n.nodes)
 	clamped[cutset] <- 1
-	crf <- clamp.crf(crf, clamped)
-	.Call("Sample_CutsetChain", crf, size)
+	newcrf <- clamp.crf(crf, clamped)
+	.Call("Sample_CutsetChain", newcrf, size)
 }
 
 sample.gibbs <- function(crf, size, burn.in = 1000, start = apply(crf$node.pot, 1, which.max))
