7d59149854871e8fe873ac0c85f1e51496e8915e
Ling-Yun Wu <wulingyun@gmail.com>
2011-07-05 21:52:36 +0800

    Add sub.crf

--- R/clamp.R
+++ R/clamp.R
@@ -44,3 +44,31 @@ clamp.reset <- function(crf, clamped)
 	crf$node.pot <- .Call("Clamp_NodePot", crf)
 	crf
 }
+
+sub.crf <- function(crf, subset)
+{
+	data <- list()
+	data$original <- crf
+
+	data$node.id <- intersect(1:crf$n.nodes, unique(subset))
+	data$n.nodes <- length(data$node.id)
+	data$node.map <- rep(0, crf$n.nodes)
+	data$node.map[data$node.id] <- 1:data$n.nodes
+
+	data$edge.id <- which(data$node.map[crf$edges[,1]] != 0 & data$node.map[crf$edges[,2]] != 0)
+	data$n.edges <- length(data$edge.id)
+	data$edges <- matrix(data$node.map[crf$edges[data$edge.id,]], ncol=2)
+	data$edge.map <- rep(0, crf$n.edges)
+	data$edge.map[data$edge.id] <- 1:data$n.edges
+
+	data <- make.adj.info(data)
+
+	data$n.states <- crf$n.states[data$node.id]
+	data$max.state <- max(data$n.states)
+
+	data$node.pot <- array(crf$node.pot[data$node.id, 1:data$max.state], dim=c(data$n.nodes, data$max.state))
+	data$edge.pot <- array(crf$edge.pot[1:data$max.state, 1:data$max.state, data$edge.id], dim=c(data$max.state, data$max.state, data$n.edges))
+
+	class(data) <- c("CRF.sub", "CRF")
+	data
+}
