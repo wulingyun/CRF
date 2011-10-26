60d46073ac85536fa9d2dd82d4364eb371138fee
Ling-Yun Wu <wulingyun@gmail.com>
2011-10-26 23:16:47 +0800

    Change the structure of edge.pot from 3-dimensional array to list of matrixes

--- R/infer.R
+++ R/infer.R
@@ -11,20 +11,20 @@ infer.conditional <- function(crf, clamped, infer.method, ...)
 {
 	belief <- list()
 	belief$node.bel <- array(0, dim=c(crf$n.nodes, crf$max.state))
-	belief$edge.bel <- array(0, dim=c(crf$max.state, crf$max.state, crf$n.edges))
+	belief$edge.bel <- lapply(1:crf$n.edges, function(i) array(0, dim=c(crf$n.states[crf$edges[i,1]], crf$n.states[crf$edges[i,2]])))
 	newcrf <- clamp.crf(crf, clamped)
 	b <- infer.method(newcrf, ...)
 	belief$node.bel[newcrf$node.id, 1:newcrf$max.state] <- b$node.bel
-	belief$edge.bel[1:newcrf$max.state, 1:newcrf$max.state, newcrf$edge.id] <- b$edge.bel
+	belief$edge.bel[newcrf$edge.id] <- b$edge.bel
 	belief$logZ <- b$logZ
 	belief$node.bel[cbind(which(clamped != 0), clamped[clamped != 0])] <- 1
 	e <- newcrf$original$edges
 	e0 <- which(clamped[e[,1]] != 0 & clamped[e[,2]] != 0)
 	e1 <- which(clamped[e[,1]] != 0 & clamped[e[,2]] == 0)
 	e2 <- which(clamped[e[,1]] == 0 & clamped[e[,2]] != 0)
-	if (length(e0) > 0) belief$edge.bel[cbind(clamped[e[e0,1]], clamped[e[e0,2]], e0)] <- 1
-	if (length(e1) > 0) belief$edge.bel[cbind(rep(clamped[e[e1,1]], each=newcrf$max.state), 1:newcrf$max.state, rep(e1, each=newcrf$max.state))] <- t(b$node.bel[newcrf$node.map[e[e1,2]],])
-	if (length(e2) > 0) belief$edge.bel[cbind(1:newcrf$max.state, rep(clamped[e[e2,2]], each=newcrf$max.state), rep(e2, each=newcrf$max.state))] <- t(b$node.bel[newcrf$node.map[e[e2,1]],])
+	for (i in e0) belief$edge.bel[[i]][clamped[e[i,1]], clamped[e[i,2]]] <- 1
+	for (i in e1) belief$edge.bel[[i]][clamped[e[i,1]],] <- b$node.bel[newcrf$node.map[e[i,2]], 1:crf$n.states[e[i,2]]]
+	for (i in e2) belief$edge.bel[[i]][,clamped[e[i,2]]] <- b$node.bel[newcrf$node.map[e[i,1]], 1:crf$n.states[e[i,1]]]
 	belief
 }
 
