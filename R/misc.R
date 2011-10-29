ef736fd8035b9faf18b94313e59b23da1e747b3b
Ling-Yun Wu <wulingyun@gmail.com>
2011-10-30 04:04:21 +0800

    Reduce the memory usage by make.crf

--- R/misc.R
+++ R/misc.R
@@ -5,7 +5,8 @@ make.crf <- function(adj.matrix, nstates)
 		stop("'adj.matrix' should be a square matrix")
 	data$n.nodes <- dim(adj.matrix)[1]
 
-	e <- which((adj.matrix + t(adj.matrix)) != 0, arr.ind = TRUE)
+	e <- which(adj.matrix != 0, arr.ind = TRUE)
+	e <- matrix(c(e, e[,2], e[,1]), ncol=2)
 	e <- matrix(e[e[,1] < e[,2],], ncol=2)
 	data$edges <- e[order(e[,1]),]
 	data$n.edges <- nrow(data$edges)
