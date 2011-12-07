3a2efff4267af0c93ca05146e98d8c1221a3a222
Ling-Yun Wu <wulingyun@gmail.com>
2011-12-07 19:56:33 +0800

    Add decode.ilp method

--- R/decode.R
+++ R/decode.R
@@ -93,3 +93,81 @@ decode.block <- function(crf, blocks, decode.method = decode.tree, restart = 0, 
 	}
 	decode
 }
+
+decode.ilp <- function(crf)
+{
+	vmap.nodes <- matrix(nrow=crf$n.nodes, ncol=2)
+	vmap.edges <- matrix(nrow=crf$n.edges, ncol=2)
+	n <- 0
+	for (i in 1:crf$n.nodes)
+	{
+		vmap.nodes[i, 1] <- n + 1
+		n <- n + crf$n.states[i]
+		vmap.nodes[i, 2] <- n
+	}
+	vnum.nodes <- n
+	for (i in 1:crf$n.edges)
+	{
+		vmap.edges[i, 1] <- n + 1
+		n <- n + crf$n.states[crf$edges[i,1]] * crf$n.states[crf$edges[i,2]]
+		vmap.edges[i, 2] <- n
+	}
+	vnum.edges <- n - vnum.nodes
+	vnum.total <- n
+
+	obj <- rep(0, vnum.total)
+	for (i in 1:crf$n.nodes)
+	{
+		obj[vmap.nodes[i,1]:vmap.nodes[i,2]] <- -log(crf$node.pot[i,1:crf$n.states[i]])
+	}
+	for (i in 1:crf$n.edges)
+	{
+		obj[vmap.edges[i,1]:vmap.edges[i,2]] <- -log(crf$edge.pot[[i]])
+	}
+
+	cnum.nodes <- crf$n.nodes
+	cnum.edges <- sum(crf$n.states[crf$edges])
+	cnum.total <- cnum.nodes + cnum.edges
+	mat <- matrix(0, nrow=cnum.total, ncol=vnum.total)
+	for (i in 1:crf$n.nodes)
+	{
+		mat[i, vmap.nodes[i,1]:vmap.nodes[i,2]] <- 1
+	}
+	n <- cnum.nodes
+	for (i in 1:crf$n.edges)
+	{
+		n1 <- crf$edges[i,1]
+		n2 <- crf$edges[i,2]
+		for (j in 1:crf$n.states[n1])
+		{
+			n <- n + 1
+			mat[n, vmap.nodes[n1,1]-1+j] <- -1
+			mat[n, seq.int(vmap.edges[i,1]-1+j, vmap.edges[i,2], by=crf$n.states[n1])] <- 1
+		}
+		for (j in 1:crf$n.states[n2])
+		{
+			n <- n + 1
+			mat[n, vmap.nodes[n2,1]-1+j] <- -1
+			mat[n, (vmap.edges[i,1]+(j-1)*crf$n.states[n1]):(vmap.edges[i,1]-1+j*crf$n.states[n1])] <- 1
+		}
+	}
+
+	rhs <- rep(0, cnum.total)
+	rhs[1:cnum.nodes] <- 1
+
+	dir <- rep("==", cnum.total)
+	types <- rep("B", vnum.total)
+	result <- Rglpk_solve_LP(obj, mat, dir, rhs, types)
+
+	if (result$status != 0)
+	{
+		warning("LP solution is not optimal.")
+	}
+
+	node.bel <- matrix(0, nrow=crf$n.nodes, ncol=crf$max.state)
+	for (i in 1:crf$n.nodes)
+	{
+		node.bel[i, 1:crf$n.states[i]] <- result$solution[vmap.nodes[i,1]:vmap.nodes[i,2]]
+	}
+	apply(node.bel, 1, which.max)
+}
