20a82c83e5889e569456482ff33f15415a8a0d86
Ling-Yun Wu <wulingyun@gmail.com>
2012-01-04 17:18:49 +0800

    Improve speed of make.adj.info by implementing using C code

---
+++ src/Make_AdjInfo.cpp
@@ -0,0 +1,72 @@
+#include "CRF.h"
+
+SEXP Make_AdjInfo(SEXP _crf)
+{
+	SEXP _nNodes, _nEdges, _edges;
+	int nNodes, nEdges, *edges;
+
+	PROTECT(_nNodes = AS_INTEGER(GetListElement(_crf, "n.nodes")));
+	PROTECT(_nEdges = AS_INTEGER(GetListElement(_crf, "n.edges")));
+	PROTECT(_edges = AS_INTEGER(GetListElement(_crf, "edges")));
+	nNodes = INTEGER_POINTER(_nNodes)[0];
+	nEdges = INTEGER_POINTER(_nEdges)[0];
+	edges = INTEGER_POINTER(_edges);
+
+	int *nAdjTemp, **adjNodesTemp, **adjEdgesTemp;
+	nAdjTemp = (int *) R_allocVector<int>(nNodes);
+	adjNodesTemp = (int **) R_allocArray<int>(nNodes, nNodes);
+	adjEdgesTemp = (int **) R_allocArray<int>(nNodes, nEdges);
+
+	int n1, n2;
+	for (int i = 0; i < nNodes; i++)
+		nAdjTemp[i] = 0;
+	for (int i = 0; i < nEdges; i++)
+	{
+		n1 = edges[i] - 1;
+		n2 = edges[i + nEdges] - 1;
+		adjNodesTemp[n1][nAdjTemp[n1]] = n2;
+		adjNodesTemp[n2][nAdjTemp[n2]] = n1;
+		adjEdgesTemp[n1][nAdjTemp[n1]] = i;
+		adjEdgesTemp[n2][nAdjTemp[n2]] = i;
+		nAdjTemp[n1]++;
+		nAdjTemp[n2]++;
+	}
+	for (int i = 0; i < nNodes; i++)
+	{
+		R_isort(adjNodesTemp[i], nAdjTemp[i]);
+		R_isort(adjEdgesTemp[i], nAdjTemp[i]);
+	}
+
+
+	SEXP _nAdj, _adjNodes, _adjEdges, _temp1, _temp2;
+	int *nAdj, *temp1, *temp2;
+	PROTECT(_nAdj = NEW_INTEGER(nNodes));
+	PROTECT(_adjNodes = NEW_LIST(nNodes));
+	PROTECT(_adjEdges = NEW_LIST(nNodes));
+	nAdj = INTEGER_POINTER(_nAdj);
+	for (int i = 0; i < nNodes; i++)
+	{
+		nAdj[i] = nAdjTemp[i];
+		PROTECT(_temp1 = NEW_INTEGER(nAdj[i]));
+		PROTECT(_temp2 = NEW_INTEGER(nAdj[i]));
+		temp1 = INTEGER_POINTER(_temp1);
+		temp2 = INTEGER_POINTER(_temp2);
+		for (int j = 0; j < nAdj[i]; j++)
+		{
+			temp1[j] = adjNodesTemp[i][j];
+			temp2[j] = adjEdgesTemp[i][j];
+		}
+		SET_VECTOR_ELT(_adjNodes, i, _temp1);
+		SET_VECTOR_ELT(_adjEdges, i, _temp2);
+	}
+
+	SEXP _adjInfo;
+	PROTECT(_adjInfo = NEW_LIST(3));
+	SetListElement(_adjInfo, 0, "n.adj", _nAdj);
+	SetListElement(_adjInfo, 1, "adj.nodes", _adjNodes);
+	SetListElement(_adjInfo, 2, "adj.edges", _adjEdges);
+
+	UNPROTECT(7 + nNodes * 2);
+
+	return(_adjInfo);
+}
