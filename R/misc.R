3c39fdcb6a4f421e8617278bf58927ee817a71d0
Ling-Yun Wu <wulingyun@gmail.com>
2012-01-10 13:55:03 +0800

    Add function train.mrf Merge features into crf data structure

--- R/misc.R
+++ R/misc.R
@@ -26,81 +26,82 @@ make.crf <- function(adj.matrix, nstates)
 	data
 }
 
-make.features <- function(crf, n.nf = 1, n.ef = n.nf)
+make.features <- function(crf, n.nf = 1, n.ef = n.nf, n.par = 1)
 {
-	data <- list()
-	data$n.nf <- n.nf
-	data$n.ef <- n.ef
-	data$node.pot <- array(0, dim=c(crf$n.nodes, crf$max.state, n.nf))
-	data$edge.pot <- lapply(1:crf$n.edges, function(i) array(0, dim=c(crf$n.states[crf$edges[i,1]], crf$n.states[crf$edges[i,2]], n.ef)))
-	data$n.params <- 0
-	data$params <- numeric(0)
-	data
+	crf$n.nf <- n.nf
+	crf$n.ef <- n.ef
+	crf$node.fea <- array(0, dim=c(crf$n.nodes, crf$max.state, n.nf))
+	crf$edge.fea <- lapply(1:crf$n.edges, function(i) array(0, dim=c(crf$n.states[crf$edges[i,1]], crf$n.states[crf$edges[i,2]], n.ef)))
+	crf$n.par <- n.par
+	crf$par <- numeric(crf$n.par)
+	crf
 }
 
-make.node.pot <- function(crf, features, instance = matrix(1, crf$n.nodes, features$n.nf))
+make.node.pot <- function(crf, instance = matrix(1, crf$n.nodes, crf$n.nf))
 {
-	map <- features$node.pot > 0
-	pot <- features$node.pot
-	pot[map] <- features$params[features$node.pot[map]]
-	node.pot <- exp(rowSums(pot * as.vector(apply(instance, 2, function(i) rep(i, crf$max.state))), dims=2))
+	index <- crf$node.fea > 0
+	feature <- crf$node.fea
+	feature[index] <- crf$par[feature[index]]
+	crf$node.pot <- exp(rowSums(feature * as.vector(apply(instance, 2, function(i) rep(i, crf$max.state))), dims=2))
+	crf
 }
 
-make.edge.pot <- function(crf, features, instance = matrix(1, crf$n.edges, features$n.ef))
+make.edge.pot <- function(crf, instance = matrix(1, crf$n.edges, crf$n.ef))
 {
 	make.edge.pot.i <- function(i)
 	{
-		map <- features$edge.pot[[i]] > 0
-		pot <- features$edge.pot[[i]]
-		pot[map] <- features$params[features$edge.pot[[i]][map]]
-		pot <- exp(rowSums(pot * rep(instance[i,], each=length(crf$edge.pot[[i]])), dims=2))
+		index <- crf$edge.fea[[i]] > 0
+		feature <- crf$edge.fea[[i]]
+		feature[index] <- crf$par[feature[index]]
+		edge.pot <- exp(rowSums(feature * rep(instance[i,], each=length(crf$edge.pot[[i]])), dims=2))
 	}
-	edge.pot <- lapply(1:crf$n.edges, make.edge.pot.i)
+	crf$edge.pot <- lapply(1:crf$n.edges, make.edge.pot.i)
+	crf
 }
 
 calc.frequency <- function(v, n)
 	.Call("Calc_Frequency", v, n)
 
-mrf.suff.stat <- function(crf, features, instances)
+mrf.suff.stat <- function(crf, instances)
 {
-	suff.stat <- numeric(features$n.params)
+	suff.stat <- numeric(crf$n.par)
 	n.instances <- dim(instances)[1]
 	index <- cbind(rep(1:crf$n.nodes, each=n.instances), as.vector(instances), rep(1, length(instances)))
-	index <- features$node.pot[index]
-	suff.stat <- suff.stat + calc.frequency(index, features$n.params)
+	index <- crf$node.fea[index]
+	suff.stat <- suff.stat + calc.frequency(index, crf$n.par)
 	for (i in 1:crf$n.edges)
 	{
 		index <- cbind(instances[,crf$edges[i,1]], instances[,crf$edges[i,2]], rep(1, n.instances))
-		index <- features$edge.pot[[i]][index]
-		suff.stat <- suff.stat + calc.frequency(index, features$n.params)
+		index <- crf$edge.fea[[i]][index]
+		suff.stat <- suff.stat + calc.frequency(index, crf$n.par)
 	}
 	suff.stat
 }
 
-mrf.nll <- function(par, crf, features, instances, infer.method=infer.chain, ...)
+mrf.nll <- function(par, crf, instances, infer.method=infer.chain, ...)
 {
 	n.instances <- dim(instances)[1]
-	features$params <- par
-	crf$node.pot <- make.node.pot(crf, features)
-	crf$edge.pot <- make.edge.pot(crf, features)
+	crf$par <- par
+	crf <- make.node.pot(crf)
+	crf <- make.edge.pot(crf)
 	belief <- infer.method(crf, ...)
-	nll <- as.vector(n.instances * belief$logZ - par %*% features$suff.stat)
+	nll <- as.vector(n.instances * belief$logZ - par %*% crf$suff.stat)
 	nll
 }
 
-mrf.nll.gradient <- function(par, crf, features, instances, infer.method=infer.chain, ...)
+mrf.nll.gradient <- function(par, crf, instances, infer.method=infer.chain, ...)
 {
 	n.instances <- dim(instances)[1]
-	features$params <- par
-	crf$node.pot <- make.node.pot(crf, features)
-	crf$edge.pot <- make.edge.pot(crf, features)
+	crf$par <- par
+	crf <- make.node.pot(crf)
+	crf <- make.edge.pot(crf)
 	belief <- infer.method(crf, ...)
-	gradient <- -features$suff.stat
+	gradient <- -crf$suff.stat
 	for (n in 1:crf$n.nodes)
 	{
 		for (s in 1:crf$n.states[n])
 		{
-			k <- features$node.pot[n,s,1]
+			k <- crf$node.fea[n,s,1]
 			if (k) gradient[k] <- gradient[k] + n.instances * belief$node.bel[n,s]
 		}
 	}
@@ -110,12 +111,21 @@ mrf.nll.gradient <- function(par, crf, features, instances, infer.method=infer.c
 		{
 			for (s2 in 1:crf$n.states[crf$edges[e,2]])
 			{
-				k <- features$edge.pot[[e]][s1,s2,1]
+				k <- crf$edge.fea[[e]][s1,s2,1]
 				if (k) gradient[k] <- gradient[k] + n.instances * belief$edge.bel[[e]][s1,s2]
 			}
 		}
 	}
 	gradient
+}
+
+train.mrf <- function(crf, rain)
+{
+	solution <- optim(crf$par, mrf.nll, mrf.nll.gradient, crf, rain, method = "L-BFGS-B")
+	crf$par <- solution$par
+	crf <- make.node.pot(crf)
+	crf <- make.edge.pot(crf)
+	crf
 }
 
 get.potential <- function(crf, configuration)
