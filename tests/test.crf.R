421b21b421846573a34d4fc71c8be14ac7e4a9ea
Ling-Yun Wu <wulingyun@gmail.com>
2012-01-28 21:53:47 +0800

    Backed out changeset: 289d01f421b7

--- tests/test.crf.R
+++ tests/test.crf.R
@@ -16,12 +16,12 @@ crf <- make.crf(adj, 2)
 crf <- make.features(crf)
 crf <- make.par(crf, 4)
 
-crf$node.par[[1]][,1] <- 1
+crf$node.par[,1,] <- 1
 for (i in 1:crf$n.edges)
 {
-	crf$edge.par[[1]][[i]][1,1] <- 2
-	crf$edge.par[[1]][[i]][1,2] <- 3
-	crf$edge.par[[1]][[i]][2,1] <- 4
+	crf$edge.par[[i]][1,1,] <- 2
+	crf$edge.par[[i]][1,2,] <- 3
+	crf$edge.par[[i]][2,1,] <- 4
 }
 
 node.fea <- lapply(1:dim(rain)[1], function(i) matrix(1, crf$n.nf, crf$n.nodes))
@@ -51,22 +51,18 @@ node.ext[[1]][2:crf$n.par] <- NaN
 
 edge.ext[[1]] <- list()
 edge.ext[[1]][[1]] <- NaN
-
 edge.ext[[1]][[2]] <- list()
 edge.ext[[1]][[2]][[1]] <- matrix(0, 2, 2)
 edge.ext[[1]][[2]][[1]][1,1] <- 1
 edge.ext[[1]][[2]][2:crf$n.edges] <- edge.ext[[1]][[2]][1]
-
 edge.ext[[1]][[3]] <- list()
 edge.ext[[1]][[3]][[1]] <- matrix(0, 2, 2)
 edge.ext[[1]][[3]][[1]][1,2] <- 1
 edge.ext[[1]][[3]][2:crf$n.edges] <- edge.ext[[1]][[3]][1]
-
 edge.ext[[1]][[4]] <- list()
 edge.ext[[1]][[4]][[1]] <- matrix(0, 2, 2)
 edge.ext[[1]][[4]][[1]][2,1] <- 1
 edge.ext[[1]][[4]][2:crf$n.edges] <- edge.ext[[1]][[4]][1]
-
 node.ext[2:dim(rain)[1]] <- node.ext[1]
 edge.ext[2:dim(rain)[1]] <- edge.ext[1]
 
@@ -86,13 +82,13 @@ crf <- make.par(crf, 16)
 
 for (i in 1:crf$n.nf)
 {
-	crf$node.par[[i]][,1] <- i
+	crf$node.par[,1,i] <- i
 }
 for (i in 1:crf$n.edges)
 {
-	crf$edge.par[[1]][[i]][1,1] <- crf$n.nf + 1
-	crf$edge.par[[1]][[i]][1,2] <- crf$n.nf + 2
-	crf$edge.par[[1]][[i]][2,1] <- crf$n.nf + 3
+	crf$edge.par[[i]][1,1,] <- crf$n.nf + 1
+	crf$edge.par[[i]][1,2,] <- crf$n.nf + 2
+	crf$edge.par[[i]][2,1,] <- crf$n.nf + 3
 }
 
 node.fea <- lapply(1:dim(rain)[1], function(i) matrix(0, crf$n.nf, crf$n.nodes))
@@ -114,14 +110,10 @@ crf$edge.pot[[1]]
 
 ##################################################
 
-crf <- make.features(crf)
 crf <- make.par(crf, 5)
 
-for (i in 1:crf$n.nf)
-{
-	crf$node.par[[i]][1,1] <- 5
-	crf$node.par[[i]][crf$n.nodes,1] <- 5
-}
+crf$node.par[1,1,] <- 5
+crf$node.par[crf$n.nodes,1,] <- 5
 
 node.fea <- lapply(1:dim(rain)[1], function(i) matrix(1, crf$n.nf, crf$n.nodes))
 edge.fea <- lapply(1:dim(rain)[1], function(i) matrix(1, crf$n.ef, crf$n.edges))
